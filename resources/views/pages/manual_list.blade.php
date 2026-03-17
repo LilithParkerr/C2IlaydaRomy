d01ec9a3a08ea77ac8e044983aaab8992041cdfd
Ilayda <159238300+EELAIDA@users.noreply.github.com>
2026-03-17 14:41:51 +0100

    css aangepast manual-btn
    de uitlijning was te breed dus ik heb de code aangepast

--- resources/views/pages/manual_list.blade.php
+++ resources/views/pages/manual_list.blade.php
@@ -1,59 +1,44 @@
 <x-layouts.app>
+    <h1>{{ $brand->name }}</h1>
 
-<h1>{{ $brand->name }}</h1>
+    <p>{{ __('introduction_texts.type_list', ['brand' => $brand->name]) }}</p>
 
-<p>{{ __('introduction_texts.type_list', ['brand' => $brand->name]) }}</p>
-
-
-@if(isset($topManuals) && $topManuals->count())
-    <h2>Top 5 populaire handleidingen</h2>
-
-      @foreach ($topManuals as $manual)
-       <button>
-           <a href="{{ route('manual.show', [
-               'brand_id' => $brand->id,
-               'brand_slug' => $brand->getNameUrlEncodedAttribute(),
-               'type_id' => $manual->type?->id ?? null,
-               'type_slug' => $manual->type?->getNameUrlEncodedAttribute() ?? null,
-               'manual_id' => $manual->id
-           ]) }}">
-               {{ $manual->name }}
-           </a>
-       </button>
-       <br>
-   @endforeach
-
-
-    <hr>
-@endif
-
-
-<h2>Alle handleidingen</h2>
-
-@foreach ($manuals as $manual)
-    @if ($manual->locally_available)
-        <a href="{{ route('manual.show', [
-            'brand_id'     => $brand->id,
-            'brand_slug'   => $brand->getNameUrlEncodedAttribute(),
-            'type_id'      => $manual->type->id,
-            'type_slug'    => $manual->type->getNameUrlEncodedAttribute(),
-            'manual_id'    => $manual->id,
-        ]) }}"
-           class="manual-btn"
-           title="{{ $manual->name }}">
-            {{ $manual->name }}
-        </a>
-        <span class="filesize">({{ $manual->filesize_human_readable }})</span>
-    @else
-
-        <button class="manuals">
-            <a href="{{ route('manual.show', ['manual_id' => $manual->id]) }}" target="_blank" title="{{ $manual->name }}">
+    @if(isset($topManuals) && $topManuals->count())
+        <p>Top 5 populaire handleidingen</p>
+        @foreach ($topManuals as $manual)
+            <a href="{{ route('manual.show', [
+                    'brand_id' => $brand->id,
+                    'brand_slug' => $manual->type?->id ? $brand->getNameUrlEncodedAttribute() : null,
+                    'type_id' => $manual->type?->id ?? null,
+                    'type_slug' => $manual->type?->getNameUrlEncodedAttribute() ?? null,
+                    'manual_id' => $manual->id
+                ]) }}"
+                class="manual-btn"
+                title="{{ $manual->name }}">
                 {{ $manual->name }}
             </a>
-        </button>
-
+            <br>
+        @endforeach
+        <hr>
     @endif
-    <br>
-@endforeach
 
+    <p>Alle handleidingen</p>
+    @foreach ($manuals as $manual)
+        <a href="{{ route('manual.show', [
+                'brand_id' => $brand->id,
+                'brand_slug' => $brand->getNameUrlEncodedAttribute(),
+                'type_id' => $manual->type?->id ?? null,
+                'type_slug' => $manual->type?->getNameUrlEncodedAttribute() ?? null,
+                'manual_id' => $manual->id,
+            ]) }}"
+            class="manual-btn"
+            title="{{ $manual->name }}"
+            @if(!$manual->locally_available) target="_blank" @endif>
+            {{ $manual->name }}
+            @if($manual->locally_available)
+                <span class="filesize">({{ $manual->filesize_human_readable }})</span>
+            @endif
+        </a>
+        <br>
+    @endforeach
 </x-layouts.app>
